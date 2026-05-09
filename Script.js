2aa6f222179acd7771fb120350a5f5674bf5d174
webbyproject <siteproject545@gmail.com>
2026-05-08 21:40:57 -0300

    08-05-2026

--- Script.js
+++ Script.js
@@ -1,58 +1,20 @@
-/* o ".value" é necessário quando se quer o valor de algum campo, se não o java entende que vc quer o código*/
-
-/*creatElement é utilizado para criar elementos, por exemplo "document.createElement("li")" - criando uma lista de tarefa com a tag <ul> na html*/
-
-/*apprendChild --- */
-
-function abaInicio () {
-
-}
-
-function cadProd () {
-
-}
-
-function abaInicio () {
-
-}
-
-function cadProd () {
-
-}
-function abaInicio () {
-
-}
-
-function formMenu(type) {
-    const inicio = document.getElementById('inicio');
-    const produtos = document.getElementById('produtos');
-    const meusProd = document.getElementById('meusProd');
-    const precos = document.getElementById('precos');
-    const config = document.getElementById('config');
-
-    if (type === 'inicio') {
-        slider.style.transform = 'translateX(-50%)';
-        indicator.style.transform = 'translateX(100%)';
-        btnRegister.classList.add('active');
-        btnLogin.classList.remove('active');
-    } else if (type === 'produtos') {
-        slider.style.transform = 'translateX(0)';
-        indicator.style.transform = 'translateX(0)';
-        btnLogin.classList.add('active');
-        btnRegister.classList.remove('active');
-    } else if (type === 'meusProd') {
-        slider.style.transform = 'translateX(0)';
-        indicator.style.transform = 'translateX(0)';
-        btnLogin.classList.add('active');
-        btnRegister.classList.remove('active');
-    } else if (type === 'precos') {
-        slider.style.transform = 'translateX(0)';
-        indicator.style.transform = 'translateX(0)';
-        btnLogin.classList.add('active');
-        btnRegister.classList.remove('active');
-    } else if (type === 'config') {
-        slider.style.transform = 'translateX(0)';
-        indicator.style.transform = 'translateX(0)';
-        btnLogin.classList.add('active');
-        btnRegister.classList.remove('active');
+function adicionarProd() {
+        window.alert("Burro")
+        let table = document.getElementById("table")
+        let linha = document.createElement("tr")
+        let produto = document.getElementById("nomeP").value;
+        let desc = document.getElementById("descP").value;
+        let categ = document.getElementById("categ").value;
+        let precProd = document.getElementById("precoP").value;
+        let precCusto = document.getElementById("precC").value;
+        let quantE = document.getElementById("quantE").value;
+        linha.innerHTML=`
+        <td>${produto}</td>
+        <td>${desc}</td>
+        <td>${categ}</td>
+        <td>${precProd}</td>
+        <td>${precCusto}</td>
+        <td>${quantE}</td>`
+        
+        table.appendChild(linha)
     }
